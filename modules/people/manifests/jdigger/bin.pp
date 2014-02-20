2c499f6686947b2691df58d7600c06eef7cc43bc
Jim Moore <moore.jim@gmail.com>
2014-02-20 14:29:27 -0500

    jdigger: Changed where the 'gw' script is installed

--- modules/people/manifests/jdigger/bin.pp
+++ modules/people/manifests/jdigger/bin.pp
@@ -3,16 +3,11 @@
 # Scripts and similiar for Jim Moore (aka "jdigger").
 #
 class people::jdigger::bin {
-  include git
-
-  $home = "/Users/${::boxen_user}"
-
-  ensure_resource('file', "${home}/bin", {ensure => 'directory'})
 
   # Gradle Wrapper finder script
-  file { "${home}/bin/gw":
+  file { "/usr/bin/gw":
     source  => 'puppet:///modules/people/jdigger/bin/gw',
-    require => File["${home}/bin"],
+    mode    => '0755',
   }
 
 }
