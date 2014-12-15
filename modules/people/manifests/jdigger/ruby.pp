0aefe5df2732f9e0448452eaa2c3d1533d9b305a
Jim Moore <moore.jim@gmail.com>
2014-12-14 20:16:52 -0700

    Updated version of AsciiDoctor

--- modules/people/manifests/jdigger/ruby.pp
+++ modules/people/manifests/jdigger/ruby.pp
@@ -13,7 +13,7 @@ class people::jdigger::ruby {
   ruby_gem { "asciidoctor for ${ruby::global::version}":
     gem          => 'asciidoctor',
     ruby_version => $ruby::global::version,
-    version      => '~> 0.1.4'
+    version      => '~> 1.5'
   }
 
 }
